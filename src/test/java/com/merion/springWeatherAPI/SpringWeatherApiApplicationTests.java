91b750b783a0ea5cd620421b92657f34467d5032
Invasiion <invassiion@yandex.ru>
2024-03-16 18:44:20 +0700

    create project

---
+++ src/test/java/com/merion/springWeatherAPI/SpringWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merion.springWeatherAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
